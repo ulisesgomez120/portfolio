632fb7b19a20d01fca200898b20d0d7eed6a41ec
Ulises Gomez <ulisesgomez120@gmail.com>
2020-09-04 18:11:54 -0500

    finished about section's structure, style, and transitions

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
+import arrowSVG from "../assets/icons/arrow.svg";
 
 const openAbout = keyframes`
   from {
@@ -21,6 +22,22 @@ const closeAbout = keyframes`
   to {
     left: 0px;
     background-color: hsl(217, 38%, 19%);
+  }
+`;
+const rotateClosed = keyframes`
+  from { 
+    transform: rotate(0deg);
+  }
+  to {
+      transform: rotate(-180deg);
+  }
+`;
+const rotateOpen = keyframes`
+  from { 
+    transform: rotate(-180deg);
+  }
+  to {
+      transform: rotate(0deg);
   }
 `;
 const Closed = styled.div`
@@ -53,20 +70,65 @@ const Li = styled.li`
   animation: ${Stagger} 350ms ${(props) => props.delay * 100 + "ms"} ease
     forwards;
 `;
+const P = styled.p`
+  color: ${({ color }) => color || "white"};
+  font-size: ${({ fontSize }) => fontSize || "16px"};
+  text-align: ${({ align }) => align || "left"};
+  margin: 30px;
+`;
+const ArrowContainer = styled.div`
+  display: flex;
+  height: 100%;
+  align-items: center;
+`;
+const Arrow = styled.img`
+  cursor: pointer;
+  &.closed {
+    animation: ${rotateClosed} 0.7s ease forwards;
+  }
+  &.open {
+    animation: ${rotateOpen} 0.7s ease forwards;
+  }
+`;
+
 const About = ({ open, toggleOpen }) => {
   return open ? (
-    <Open onClick={toggleOpen}>
-      Open
-      <ul>
-        <Li delay=".5">Gold test</Li>
-        <Li delay="2">pink test</Li>
-        <Li delay="3">pink test</Li>
-        <Li delay="2">pink test</Li>
-      </ul>
+    <Open>
+      <h2 style={{ color: "#17191c" }}>About</h2>
+      <div style={{ display: "flex", height: "100%" }}>
+        <ArrowContainer>
+          <Arrow src={arrowSVG} onClick={toggleOpen} className="open" />
+        </ArrowContainer>
+        <div>
+          <P>A firm believer in</P>
+          <P fontSize="1.4em" color="#f7d891" align="right" margin>
+            Getting After It!
+          </P>
+          <P>and answering most problems with one word</P>
+          <P fontSize="1.6em" color="#f7d891" align="right">
+            GOOD.
+          </P>
+          <P>
+            I can work through out the web stack, but enjoy working on the
+            front-end the most. I especially like{" "}
+            <span style={{ color: "#61DBFB" }}>React</span> and have been using
+            styled-components lately. A full list of my skills can be found on
+            my resume --{">"} Ulises_Gomez.pdf
+          </P>
+          <P>
+            Apart from a programmer, I am also a Husband, a gammer, and, once
+            Covid is clear, I want to practice jiu-jitsu. I am always looking
+            for new challenges and believe self development is very important.
+          </P>
+        </div>
+      </div>
     </Open>
   ) : (
-    <Closed onClick={toggleOpen}>
+    <Closed>
       <h2>About</h2>
+      <ArrowContainer>
+        <Arrow src={arrowSVG} onClick={toggleOpen} className="closed" />
+      </ArrowContainer>
     </Closed>
   );
 };
